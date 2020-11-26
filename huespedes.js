b8ffebabc81c23b738e62d06bdaa3f76694f9b55
gri-espindola <griespindola@hotmail.com>
2020-11-25 23:44:21 -0300

    Cantidad de huespedes ejercicio

---
+++ huespedes.js
@@ -0,0 +1,12 @@
+const habitacion2 = prompt("¿Cuantas habitaciones para dos personas tiene?");
+const habitacion3 = prompt("¿Cuantas habitaciones para tres personas tiene?");
+const habitacion4 = prompt("¿Cuantas habitaciones para cuatro personas tiene?");
+console.log (typeof habitacion2);
+const personas2 = habitacion2 * 2;
+const personas3 = habitacion3 * 3;
+const personas4 = habitacion4 * 4;
+console.log (typeof personas2);
+const resultado = personas2 + personas3 + personas4;
+
+console.log (resultado)
+
